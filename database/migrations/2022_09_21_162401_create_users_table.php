cfd43fdf6fc6f4f6058cbfc6ad9b90b766d12cf1
Aleksandar Zarin <AcErOOy@github.com>
2023-03-01 16:51:14 +0100

    AleksandarZarin

---
+++ database/migrations/2022_09_21_162401_create_users_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->bigIncrements("id");
+            $table->string("username", 180);
+            $table->string("password", 180);
+            $table->string("email", 180);
+            $table->string("name", 180);
+            $table->string("phone", 180);
+            $table->date("birth_date");
+            $table->unsignedBigInteger('role_id');
+            $table->foreign('role_id')->references('id')->on('roles')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+};
